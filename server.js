cf02696e88191b7a5c9900b5f0d55068e3aff8c3
Hasala Dissanayake <hasala@linearsix.com>
2023-11-08 11:11:46 +0530

    completed express server for having API requests for post, put, get, delete requests

--- server.js
+++ server.js
@@ -1,19 +1,84 @@
-const http = require('http');
+const express = require("express");
+const Joi = require('joi');
 
-const PORT = 3000
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.write("Hello World");
-        res.end();
-    }
+// middleware to request processing
+app.use(express.json());
 
-    if(req.url === '/api/courses') {
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-server.listen(PORT);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-console.log(`Listening on port ${PORT}`);
+app.post("/api/courses", (req, res) => {
+
+  const { error } = validateCourse(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/course/:id', (req, res)=> {
+  //to find the course
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("The course with the given ID was not found");
+
+  const { error } = validateCourse(req.body); //result.error
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+  
+});
+
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  console.log(course);
+  if (!course) return res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+
+});
+
+// validation logic
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string()
+    .min(3)
+    .required()
+  })
+
+  return schema.validate({name: course.name});
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
